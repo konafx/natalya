ced3b4505f32f3e930cfe52c33ef98b114d16bcb
konafx <afxlieonon@gmail.com>
2021-05-15 00:18:19 +0900

    Haiku DONE

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	cloud.google.com/go/firestore v1.5.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
 	github.com/bwmarrin/discordgo v0.23.3-0.20210410202908-577e7dd4f6cc
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
--- haiku.go
+++ haiku.go
@@ -4,30 +4,43 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"math"
+	"regexp"
 	"sort"
 	"strconv"
 	"time"
-
-	"github.com/bwmarrin/discordgo"
-	log "github.com/sirupsen/logrus"
+	"unicode/utf8"
 
 	"cloud.google.com/go/firestore"
+	"github.com/bwmarrin/discordgo"
 	u "github.com/konafx/natalya/util"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/api/iterator"
-	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+// docID: increment
 type Poem struct {
-	Next	int			`json:"next" firestore:"next"`
-	UserID	string		`json:"userId" firestore:"userId"`
 	Poem	string		`json:"poem" firestore:"poem"`
 }
 
+// docID: increment
+type Poet struct {
+	Next	int			`json:"next" firestore:"next"`
+	UserID	string		`json:"userId" firestore:"userId"`
+}
+
+// docID: UserID
+type UnknownPoetGamePlayer struct {
+	PlayingGameID	string	`json:"playingGameId" firestore:"playingGameId"`
+}
+
+// docID: auto
 type UnknownPoetGame struct {
-	GuildID	string		`json:"guildId" firestore:"guildId"`
-	UserIDs	[]string	`json:"userIds" firestore:"userIds"`
-	Stage	int			`json:"stage" firestore:"stage"`
+	GuildID			string		`json:"guildId" firestore:"guildId"`
+	NumberOfPlayers	int			`json:"numberOfPlayers" firestore:"numberOfPlayers"`
+	Stage			int			`json:"stage" firestore:"stage"`
 }
 
 var Haiku Command = &discordgo.ApplicationCommand{
@@ -98,34 +111,39 @@ var Haiku Command = &discordgo.ApplicationCommand{
 }
 
 func HaikuHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	var poems []Poem
+	var poets []Poet
 	for k, v := range i.Data.Options {
 		u := v.UserValue(s)
-		poem := Poem{
+		poet := Poet{
 			Next:	k,
 			UserID:	u.ID,
 		}
-		poems = append(poems, poem)
+		poets = append(poets, poet)
 	}
-	log.Debugln(poems)
+	log.Debugln(poets)
 
-	// TODO: 同じユーザーが2つ以上のゲームをできないようにするフィルター、データ構造
-	var userIds []string
-	for _, v := range poems {
-		userIds = append(userIds, v.UserID)
-	}
-	sort.Slice(userIds, func(i, j int) bool { return userIds[i] < userIds[j] })
-	for k, v := range userIds {
-		if k == len(userIds) {
-			break
+	// 重複チェック
+	{
+		var userIds []string
+		for _, v := range poets {
+			userIds = append(userIds, v.UserID)
 		}
-		if v == userIds[k+1] {
-			u.InteractionErrorResponse(s, i.Interaction, fmt.Sprintf("%s が二回指名されてるゾ", u.ToUser(v)))
-			return
+		sort.Slice(userIds, func(i, j int) bool { return userIds[i] < userIds[j] })
+		for k, v := range userIds {
+			log.Debugln(k, v)
+			if k + 1 == len(userIds) {
+				break
+			}
+			if v == userIds[k+1] {
+				u.InteractionErrorResponse(s, i.Interaction, fmt.Sprintf("%s が二回指名されてるゾ", u.ToUser(v)))
+				return
+			}
 		}
 	}
+
+	// 順番ランダム
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(poems), func(i, j int) { poems[i], poems[j] = poems[j], poems[i] })
+	rand.Shuffle(len(poets), func(i, j int) { poets[i], poets[j] = poets[j], poets[i] })
 
 	ctx := context.Background()
 	client := createClient(ctx)
@@ -135,7 +153,8 @@ func HaikuHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	{
 		game := UnknownPoetGame{
 			GuildID:	i.GuildID,
-			UserIDs:	userIds,
+			NumberOfPlayers: len(poets),
+			Stage:		1,
 		}
 		doc, _, err := client.Collection("unknownPoetGames").Add(ctx, game)
 		if err != nil {
@@ -143,6 +162,28 @@ func HaikuHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			log.Error(err)
 			return
 		}
+
+		// TODO: 同じユーザーが2つ以上のゲームをできないようにするフィルター、データ構造
+		for _, v := range poets {
+			player := UnknownPoetGamePlayer{
+				PlayingGameID: doc.ID,
+			}
+			if _, err := client.Collection("unknownPoetGamePlayers").Doc(v.UserID).Set(ctx, player); err != nil {
+				u.InteractionErrorResponse(s, i.Interaction, "ウーン、俳句を詠む心が備わってないないみたイ…")
+				log.Error(err)
+				return
+			}
+		}
+
+		for k, v := range poets {
+			if _, err := doc.Collection("poets").Doc(strconv.Itoa(k)).Set(ctx, v); err != nil {
+				u.InteractionErrorResponse(s, i.Interaction, "ウーン、俳句を詠む心が備わってないないみたイ…")
+				log.Error(err)
+				return
+			}
+		}
+
+		poems := make([]Poem, len(poets))
 		for k, v := range poems {
 			if _, err := doc.Collection("poems").Doc(strconv.Itoa(k)).Set(ctx, v); err != nil {
 				u.InteractionErrorResponse(s, i.Interaction, "ウーン、俳句を詠む心が備わってないないみたイ…")
@@ -154,7 +195,7 @@ func HaikuHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	}
 
 	message := "今回の俳人はお前らダ！よろしくナ♪\n"
-	for k, v := range poems {
+	for k, v := range poets {
 		if k == 0 {
 			message = fmt.Sprintf("%s%s", message, u.ToUser(v.UserID))
 			continue
@@ -175,23 +216,30 @@ func HaikuHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		},
 	})
 
+	log.Debugln("game start")
 	// ゲーム開始
 	{
 		c := make(chan struct{})
-		waitFinishWritingPoems(s, ctx, gamedoc, c)
+		log.Debugln("Start to wait writing poems")
+		go waitFinishWritingPoems(s, ctx, gamedoc, c)
 		_, ok := <-c
+		log.Debugln("End to wait writing poems")
 		if !ok {
 			u.InteractionErrorResponse(s, i.Interaction, "なんか起きたので中断されました")
 			return
 		}
+		// ゲーム終了後はresult としてどっか別のDBに保存 and game からは消す
 	}
 
-	poems2, _ := getPoems(ctx, gamedoc)
+	poems, _ := getPoems(ctx, gamedoc)
 
 	result := "俳句ができたヨ\n"
-	for _, v := range poems2 {
-		result = fmt.Sprintf("%s\n%s %s", result, u.ToUser(v.UserID), v.Poem)
+	for i, l := 0, len(poems); i<l; i++ {
+		result = fmt.Sprintf("%s\n%s %s", result, u.ToUser(poets[i].UserID), poems[i].Poem)
 	}
+	log.Debug(result)
+
+	s.ChannelMessageSend(i.ChannelID, result)
 
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -210,41 +258,48 @@ func HaikuHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 func waitFinishWritingPoems(s *discordgo.Session, ctx context.Context, gamedoc *firestore.DocumentRef, c chan struct{}) {
 	defer close(c)
 
-	poems, err := getPoems(ctx, gamedoc)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	// DM を送る
-	for _, v := range poems {
-		if err = sendDraftPoem(s, ctx, gamedoc, v); err != nil {
+	// 各ステージの終了を検知する
+	for {
+		poets, err := getPoets(ctx, gamedoc)
+		if err != nil {
 			log.Error(err)
 			return
 		}
-	}
+		log.Debugln(poets)
 
-	// 各ステージの終了を検知する
-	for {
+		// DM を送る
+		for _, v := range poets {
+			if err = sendDraftPoem(s, ctx, gamedoc, v); err != nil {
+				log.Error(err)
+				return
+			}
+		}
+
 		c2 := make(chan int)
-		waitFinishStage(ctx, gamedoc, c2)
+		log.Debugln("Start to wait finish stage")
+		go waitFinishStage(ctx, gamedoc, c2)
 		stage, ok := <-c2
+		log.Debugln("End to wait finish stage")
 		if !ok {
-			log.Errorf("something happened on waitFinishStage")
+			log.Error("something happened on waitFinishStage")
 			return
 		}
 
 		stage = stage + 1
 		// TODO stage 更新処理
-		gamedoc.Update(ctx, []firestore.Update{
+		_, err = gamedoc.Update(ctx, []firestore.Update{
 			{
-				Path: "Stage",
+				Path: "stage",
 				Value: stage,
 			},
 		})
+		if err != nil {
+			log.Error(err)
+			return
+		}
 
-		// stage = [0, 16]
-		if stage > 16 {
+		// stage = [1, 17]
+		if stage > 3 {
 			c <- struct{}{}
 			return
 		}
@@ -255,8 +310,14 @@ func waitFinishWritingPoems(s *discordgo.Session, ctx context.Context, gamedoc *
 func waitFinishStage(ctx context.Context, gamedoc *firestore.DocumentRef, c chan int) {
 	defer close(c)
 
-	ctx2, cancel := context.WithTimeout(ctx, 30*time.Second)
+	ctx2, cancel := context.WithTimeout(ctx, 120*time.Second)
 	defer cancel()
+
+	var game UnknownPoetGame
+	{
+		snap, _ := gamedoc.Get(ctx)
+		snap.DataTo(&game)
+	}
 
 	it := gamedoc.Collection("poems").Snapshots(ctx2)
 	for {
@@ -269,17 +330,15 @@ func waitFinishStage(ctx context.Context, gamedoc *firestore.DocumentRef, c chan
 			log.Error(err)
 			return
 		}
-		gamesnap, err := gamedoc.Get(ctx)
-		var game UnknownPoetGame
-		gamesnap.DataTo(&game)
+		log.Debug("catch change poem")
 		if snap != nil {
 			iter := gamedoc.Collection("poems").Documents(ctx)
 			for {
 				doc, err := iter.Next()
 				if err == iterator.Done {
+					log.Info("waitFinish!!")
 					c <- game.Stage
-					return
-					// ok
+					return // ok
 				}
 				if err != nil {
 					log.Errorf("Documents.Next: %v", err)
@@ -287,9 +346,9 @@ func waitFinishStage(ctx context.Context, gamedoc *firestore.DocumentRef, c chan
 				}
 				var poem Poem
 				doc.DataTo(&poem)
-				// ひとつでも足りなければフェーズ続行
+				log.Debug(poem.Poem, utf8.RuneCountInString(poem.Poem), game.Stage)
 				// TODO: 拗音対応
-				if len(poem.Poem) < game.Stage {
+				if utf8.RuneCountInString(poem.Poem) < game.Stage {
 					break
 				}
 			}
@@ -298,19 +357,20 @@ func waitFinishStage(ctx context.Context, gamedoc *firestore.DocumentRef, c chan
 }
 
 // 各ユーザーにDMおくって放置
-func sendDraftPoem(s *discordgo.Session, ctx context.Context, gamedoc *firestore.DocumentRef, poem *Poem) error {
-	snap, err := gamedoc.Collection("poems").Doc(strconv.Itoa(poem.Next)).Get(ctx)
-	if err != nil {
-		return err
-	}
-	var target Poem
-	snap.DataTo(&target)
-
+func sendDraftPoem(s *discordgo.Session, ctx context.Context, gamedoc *firestore.DocumentRef, poet *Poet) error {
 	// send dm
-	ch, err := s.UserChannelCreate(target.UserID)
+	ch, err := s.UserChannelCreate(poet.UserID)
 	if err != nil {
 		return err
 	}
+
+	snap, err := gamedoc.Collection("poems").Doc(strconv.Itoa(poet.Next)).Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	var poem Poem
+	snap.DataTo(&poem)
 
 	var message string
 	if len(poem.Poem) == 0 {
@@ -339,42 +399,46 @@ func UnknownPoetDMHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	ctx := context.Background()
 	client := createClient(ctx)
 
-	log.Info("get game")
-	var gameRef *firestore.DocumentRef
+	log.Info("get player")
+	var player UnknownPoetGamePlayer
 	{
-		it := client.Collection("unknownPoetGames").Where("userIds", "array-contains", m.Author.ID).Documents(ctx)
-		defer it.Stop()
-
-		for {
-			doc, err := it.Next()
-			if err == iterator.Done {
-				return
-			}
-			if err != nil {
-				log.Error(err)
-				s.ChannelMessageSendReply(m.ChannelID, "エラーだヨ…", m.Reference())
-				return
-			}
-			gameRef = doc.Ref
-			break
+		snap, err := client.Collection("unknownPoetGamePlayers").Doc(m.Author.ID).Get(ctx)
+		if status.Code(err) == codes.NotFound {
+			return
 		}
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		snap.DataTo(&player)
 	}
 
 	// 参加者であることがおそらく保証されている
-	// TODO: ここらへんで m.Content が一文字だけか、ひらがなかチェックしていこう
-	if len(m.Content) != 1 {
+	log.Info("get game")
+	var gamedoc *firestore.DocumentRef
+	{
+		snap, err := client.Collection("unknownPoetGames").Doc(player.PlayingGameID).Get(ctx)
+		if err != nil {
+			log.Error(err)
+			s.ChannelMessageSendReply(m.ChannelID, "エラーだヨ…", m.Reference())
+			return
+		}
+		gamedoc = snap.Ref
+	}
+
+	if !regexp.MustCompile(`^\p{Hiragana}$`).MatchString(m.Content) {
 		s.ChannelMessageSendReply(m.ChannelID, "1文字にしてね", m.Reference())
 		return
 	}
 
-	var poem Poem
-	log.Info("get your poem")
+	log.Info("get poet")
+	var poetsnap *firestore.DocumentSnapshot
 	{
-		it := gameRef.Collection("poems").Where("userId", "==", m.Author.ID).Documents(ctx)
+		it := gamedoc.Collection("poets").Where("userId", "==", m.Author.ID).Documents(ctx)
 		defer it.Stop()
 
 		for {
-			doc, err := it.Next()
+			snap, err := it.Next()
 			if err == iterator.Done {
 				log.Error("Cannot find poem in game played by dm user")
 				return
@@ -384,54 +448,81 @@ func UnknownPoetDMHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				s.ChannelMessageSendReply(m.ChannelID, "エラーだヨ…", m.Reference())
 				return
 			}
-			doc.DataTo(&poem)
+			poetsnap = snap
 			break
 		}
 	}
+	var poet Poet
+	poetsnap.DataTo(&poet)
 
-	// poem が格納されている document の ID
-	var ID string
+	log.Infof("get poem %d", poet.Next)
+	var poem Poem
 	{
-		docsnap, err := gameRef.Collection("poems").Doc(strconv.Itoa(poem.Next)).Get(ctx)
+		snap, err := gamedoc.Collection("poems").Doc(strconv.Itoa(poet.Next)).Get(ctx)
 		if err != nil {
-			log.Error(err)
+			s.ChannelMessageSendReply(m.ChannelID, "エラーだヨ…", m.Reference())
 			return
 		}
-		pmap := docsnap.Data()
-		for k, v := range pmap {
-			ID = k
-			poem = v.(Poem)
-		}
+		
+		snap.DataTo(&poem)
 	}
-	log.Debugf("docID: %s, poem: %v", ID, poem)
+
+	log.Debug(poet, poem)
 
 	var game UnknownPoetGame
 	{
-		docsnap, err := gameRef.Get(ctx)
+		snap, err := gamedoc.Get(ctx)
 		if err != nil {
 			log.Error(err)
 			return
 		}
-		docsnap.DataTo(&game)
+		snap.DataTo(&game)
 	}
 
 	// write
-	if len(game.UserIDs) >= poem.Next {
-		poem.Next = 0
-	} else {
-		poem.Next = poem.Next + 1
-	}
-
-	poem.Poem = poem.Poem + m.Content
+	poem.Poem += m.Content
 
 	// update
-	_, err := gameRef.Collection("poems").Doc(ID).Set(ctx, poem)
+	log.Info("update poem and poet")
+	_, err := gamedoc.Collection("poems").Doc(strconv.Itoa(poet.Next)).Set(ctx, poem)
 	if err != nil {
 		log.Error(err)
 		return
 	}
+	_, err = poetsnap.Ref.Update(ctx, []firestore.Update{
+		{
+			Path: "next",
+			Value: int(math.Mod(float64(poet.Next + 1), float64(game.NumberOfPlayers))),
+		},
+	})
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
 	s.ChannelMessageSendReply(m.ChannelID, "しばしお待ちを", m.Reference())
 
+	return
+}
+
+func getPoets(ctx context.Context, gamedoc *firestore.DocumentRef) (poets []*Poet, err error) {
+	it := gamedoc.Collection("poets").Documents(ctx)
+	defer it.Stop()
+
+	for {
+		var snap *firestore.DocumentSnapshot
+		snap, err = it.Next()
+		if err == iterator.Done {
+			err = nil
+			break
+		}
+		if err != nil {
+			return
+		}
+		var poet Poet
+		snap.DataTo(&poet)
+		poets = append(poets, &poet)
+	}
 	return
 }
 
@@ -443,6 +534,7 @@ func getPoems(ctx context.Context, gamedoc *firestore.DocumentRef) (poems []*Poe
 		var snap *firestore.DocumentSnapshot
 		snap, err = it.Next()
 		if err == iterator.Done {
+			err = nil
 			break
 		}
 		if err != nil {
@@ -452,6 +544,15 @@ func getPoems(ctx context.Context, gamedoc *firestore.DocumentRef) (poems []*Poe
 		snap.DataTo(&poem)
 		poems = append(poems, &poem)
 	}
+	return
+}
+
+func getGame(ctx context.Context, gamedoc *firestore.DocumentRef) (game *UnknownPoetGame, err error) {
+	snap, err := gamedoc.Get(ctx)
+	if err != nil {
+		return
+	}
+	snap.DataTo(&game)
 	return
 }
 
